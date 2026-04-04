9be60fa86535ac4f0219077115698e26665d949c
BajiLinux <mr.baji.smart@gmail.com>
2026-04-04 20:18:34 +0700

    Done

--- readme.md
+++ readme.md
@@ -1,1 +1,17 @@
-Jangan lupa untuk menghapus folder node_modules sebelum mengirimkan submission
+<h1>Helow ini adalah project submisson dicoding reactjs dan ini hasil dari ane sendiri coy</h1>
+
+```bash
+git clone https://bajiff/baji-notes.git
+```
+
+```bash
+cd baji-notes
+```
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,9 @@
-// ? index.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import ProfileCard from './challenges/profile-card/ProfileCard.jsx';
-// import InventoryList from './challenges/array-function/InventoryList';
-// import inventoryItems from './challenges/array-function/inventoryItems';
-// import PotionButton from './challenges/state-alchemist/PotionButton';
-// import ManaPump from './challenges/state-alchemist/ManaPump';
-// import GuildForm from './challenges/guild-form/GuildForm';
-// import App from './challenges/final-boss/App';
 import App from './components/App.jsx';
+
 // import style
 import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<App />);
